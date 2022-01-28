e135fbcbfe5f5402b4816115d7faaf6a43cde121
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-01-27 22:01:14 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá flutter'),
+      ),
+    );
+  }
+}
